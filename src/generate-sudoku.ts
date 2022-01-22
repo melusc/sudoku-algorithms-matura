c13fd9c0bfcc57e9ea736046e49066bcfb7e656d
Luca <lusc.dev@gmail.com>
2022-01-22 23:08:04 +0100

    Add generate-sudoku

---
+++ src/generate-sudoku.ts
@@ -0,0 +1,80 @@
+import {randomInt} from 'node:crypto';
+import assert from 'node:assert/strict';
+
+import {Sudoku} from '@lusc/sudoku';
+
+// This function generates valid sudokus
+// by iterating through all cells from start and from back simultaneously
+// and trying to randomly fill from `possibles`.
+export const generateFilledSudoku = (size: number): Sudoku => {
+	assert(Number.isInteger(size) && size > 0);
+
+	let sudoku = new Sudoku();
+	sudoku.shouldLogErrors = false;
+
+	/**
+	 * For the given index try randomly from
+	 * `possibles` until one is valid
+	 */
+	const tryFillCell = (index: number): void => {
+		if (sudoku.getContent(index) !== undefined) {
+			return;
+		}
+
+		const allPossibles = [...sudoku.getCell(index).possible];
+
+		while (!sudoku.isSolved() && allPossibles.length > 0) {
+			const randIndex = randomInt(allPossibles.length);
+			const randNumber = allPossibles[randIndex]!;
+			const newSudoku = sudoku.clone();
+
+			newSudoku.setContent(index, randNumber);
+
+			if (newSudoku.solve() !== 'error' && newSudoku.isValid()) {
+				sudoku = newSudoku;
+				return;
+			}
+
+			allPossibles.splice(randIndex, 1);
+		}
+	};
+
+	// prettier-ignore
+	const maxIndex = (size ** 2) - 1;
+	for (let i = 0; !sudoku.isSolved() && i <= maxIndex / 2; ++i) {
+		tryFillCell(i);
+		tryFillCell(maxIndex - i);
+	}
+
+	if (sudoku.isSolved() && sudoku.isValid()) {
+		return sudoku;
+	}
+
+	return generateFilledSudoku(size);
+};
+
+// This function gets a filled sudoku
+// and randomly removes cells until the sudoku cannot
+// be solved by `@lusc/sudoku` anymore
+export const generateSudoku = (size: number): Sudoku => {
+	assert(Number.isInteger(size) && size > 0);
+
+	const sudoku = generateFilledSudoku(size);
+
+	const indices = Array.from({length: size ** 2}, (_v, i) => i);
+
+	while (indices.length > 0) {
+		const newSudoku = sudoku.clone();
+		const randIndex = randomInt(indices.length);
+
+		const validity = newSudoku.clearCell(randIndex).solve();
+
+		if (validity === 'finish') {
+			sudoku.clearCell(randIndex);
+		}
+
+		indices.splice(randIndex, 1);
+	}
+
+	return sudoku;
+};
