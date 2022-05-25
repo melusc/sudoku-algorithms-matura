9b0bdc0db4378625583cf577ecc0a23bb17f8c19
Luca <lusc.dev@gmail.com>
2022-05-25 22:31:26 +0200

    Pass combinations to stats

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
+import {getSudokus} from './cached-sudokus.js';
 import {stats} from './stats/index.js';
+import {tryCombinations} from './try-combinations.js';
+
+const sudokuAmount = 2500;
 
 for (let i = 1; i <= 5; ++i) {
-	// eslint-disable-next-line no-await-in-loop
-	await stats(i);
+	for (const size of [9, 16] as const) {
+		// eslint-disable-next-line no-await-in-loop
+		const combinations = await tryCombinations(
+			size,
+			sudokuAmount,
+			async () => getSudokus(sudokuAmount, size),
+			sudokuAmount,
+		);
+
+		// eslint-disable-next-line no-await-in-loop
+		await Promise.all([stats(combinations, size)]);
+	}
 }
--- src/stats/index.ts
+++ src/stats/index.ts
@@ -1,29 +1,18 @@
-import {getSudokus} from '../cached-sudokus.js';
-import {tryCombinations} from '../try-combinations.js';
+import {CombinationsResults} from '../try-combinations.js';
 
 import {amountSolved} from './amount-solved.js';
 import {rounds} from './rounds.js';
 import {unsolved} from './unsolved.js';
 
-export const stats = async (combinationsAmount: number) => {
-	const sudokuAmount = 2500;
+export const stats = async (
+	combinations: CombinationsResults,
+	size: number,
+) => {
+	const promises: Array<Promise<void>> = [];
 
-	for (const size of [9, 16] as const) {
-		// eslint-disable-next-line no-await-in-loop
-		const result = await tryCombinations(
-			size,
-			combinationsAmount,
-			async () => getSudokus(sudokuAmount, size),
-			sudokuAmount,
-		);
-
-		const promises: Array<Promise<void>> = [];
-
-		for (const fn of [rounds, amountSolved, unsolved]) {
-			promises.push(fn(result, size));
-		}
-
-		// eslint-disable-next-line no-await-in-loop
-		await Promise.all(promises);
+	for (const fn of [rounds, amountSolved, unsolved]) {
+		promises.push(fn(combinations, size));
 	}
+
+	await Promise.all(promises);
 };
